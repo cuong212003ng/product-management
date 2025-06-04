c1d4eddac2a7ecc94e13e18c12cd6fa33232734e
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-06-04 17:52:22 +0700

    update

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,11 +1,19 @@
 const Product = require("../../models/product.model")
 module.exports.index = async (req, res) => {
-    const products = await Product.find({});
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
     console.log(products);
     
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0)
+        return item
+    })
+
     res.render("client/pages/product/index", {
         titlePage: "Danh sách sản phẩm",
-        products: products
+        products: newProducts
     })
 }
 
