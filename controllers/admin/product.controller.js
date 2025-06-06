c249dad7b996be15d1d824b6cdf4e6d454391502
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-06-06 13:28:18 +0700

    update

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,13 @@
+const Product = require("../../models/product.model")
 // [GET] /admin/products
-module.exports.product = (req, res) => {
+module.exports.product = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+    console.log(products);   
+
     res.render("admin/pages/product/index", {
-        titlePage: "Dashboard"
+        titlePage: "Danh sách sản phẩm",
+        products: products
     })
 }
