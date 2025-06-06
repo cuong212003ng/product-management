b78b233d46e536e0bda6ce4c4e4bec0752abfc14
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-06-06 15:48:16 +0700

    update chuc nang tim kiem

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,13 +1,47 @@
 const Product = require("../../models/product.model")
 // [GET] /admin/products
 module.exports.product = async (req, res) => {
-    const products = await Product.find({
+    // console.log(req.query.status);
+
+    let fillterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+
+    let find = {
         deleted: false
-    });
-    console.log(products);   
+    }
+
+    if(req.query.status) {
+        const index = fillterStatus.findIndex(item => item.status === req.query.status)
+        fillterStatus[index].class = "active"
+    }else {
+        const index = fillterStatus.findIndex(item => item.status === "")
+        fillterStatus[index].class = "active"
+    }
+
+    if(req.query.status) {
+        find.status = req.query.status
+    }
+    const products = await Product.find(find);
+    // console.log(products);   
 
     res.render("admin/pages/product/index", {
         titlePage: "Danh sách sản phẩm",
-        products: products
+        products: products,
+        fillterStatus: fillterStatus
     })
 }
